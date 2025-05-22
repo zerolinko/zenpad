22b150b84394aaf1ba5f9ea5962d8ec895cd1947
zerolinko <csgopovdemo@gmail.com>
2025-05-22 23:53:12 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import ConnectWallet from './components/ConnectWallet';
+import CreateToken from './pages/CreateToken';
 
 export default function App() {
   return (
     <div className="p-4">
-      <h1 className="text-2xl font-bold text-green-500">ZenPad</h1>
-      <ConnectWallet />
+      <h1 className="text-2xl font-bold text-green-500 mb-4">ZenPad</h1>
+      <CreateToken />
     </div>
   );
 }
