f1da24fd7182727f0281294761084fcb0b6753e9
zerolinko <csgopovdemo@gmail.com>
2025-05-25 01:30:06 +0300

    Update TokenFactory.sol

--- src/contracts/TokenFactory.sol
+++ src/contracts/TokenFactory.sol
@@ -2,20 +2,14 @@
 pragma solidity ^0.8.0;
 
 import "./BondingToken.sol";
-import "./IERC20.sol";
 
 contract TokenFactory {
     address public feeCollector = 0x6c2Bd887389Fe2f8Df6cBEe1A6FbEd6C0dD99154;
-    uint256 public creationFee = 100 * 1e18; // 100 ZTC
-    address public ztcToken;
+    uint256 public creationFee = 100 ether;
 
     address[] public tokens;
 
     event TokenCreated(address token, address creator);
-
-    constructor(address _ztcToken) {
-        ztcToken = _ztcToken;
-    }
 
     function createToken(
         string memory name,
@@ -24,12 +18,12 @@ contract TokenFactory {
         string memory imageURI,
         uint256 totalSupply,
         uint256 initialBuyPercent
-    ) external {
+    ) external payable {
+        require(msg.value == creationFee, "Require exactly 100 ZTC");
         require(totalSupply <= 10_000_000, "Max supply exceeded");
-        require(initialBuyPercent >= 1 && initialBuyPercent <= 100, "Initial buy must be between 1 and 100");
+        require(initialBuyPercent >= 1 && initialBuyPercent <= 100, "Initial buy must be 1%-100%");
 
-        // Transfer creation fee
-        IERC20(ztcToken).transferFrom(msg.sender, feeCollector, creationFee);
+        payable(feeCollector).transfer(msg.value);
 
         BondingToken token = new BondingToken(
             name,
@@ -38,23 +32,17 @@ contract TokenFactory {
             imageURI,
             msg.sender,
             totalSupply,
-            ztcToken,
             feeCollector
         );
 
         uint256 initialAmount = (totalSupply * initialBuyPercent) / 100;
-        token.buy{value: 0}(msg.sender, initialAmount);
+        token.initialBuy{value: initialAmount * 1 ether}(msg.sender, initialAmount);
 
         tokens.push(address(token));
-
         emit TokenCreated(address(token), msg.sender);
     }
 
     function getTokens() external view returns (address[] memory) {
         return tokens;
-    }
-
-    function getTokenCount() external view returns (uint256) {
-        return tokens.length;
     }
 }
